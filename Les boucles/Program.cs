15943714d56e78afd94e583fecf4401997624e31
omayma hajjami <omayma.hajjami@gmail.com>
2020-12-01 16:22:36 +0100

    afficher la moitié de la valeur

--- Les boucles/Program.cs
+++ Les boucles/Program.cs
@@ -8,14 +8,30 @@ namespace Les_boucles
         {
             int a = 1;
             int c = 1;
-           Random random = new Random();
+            Random random = new Random();
             int randomNumber;
-            randomNumber = random.Next(2, 200);  
+            randomNumber = random.Next(2, 200);
             while (a <= 30)
             {
                 c = a * randomNumber;
                 Console.WriteLine(c);
                 a++;
+            }
+
+        }
+    }
+
+    class moitie
+    {
+        static void Main(string[] args)
+        {
+           float a = 1F;
+           
+           
+            while (a <= 10)
+            {
+                Console.WriteLine(a);
+                a = a + a/2;
             }
 
         }
