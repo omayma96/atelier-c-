6ccc9b852b9faac03ca1e477f55858d443bbc63c
omayma hajjami <omayma.hajjami@gmail.com>
2020-12-01 14:38:14 +0100

    exercice 1 done !!

---
+++ Les variables/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Les_variables
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("entrer le nom du produit");
+                string nomProduit = Console.ReadLine();
+            Console.WriteLine("entrer le prix du produit");
+                double prixHorsTva = Convert.ToDouble(Console.ReadLine());
+            int tva = 19;
+            double dTva = Convert.ToDouble(tva);
+            double prixTtc = prixHorsTva + prixHorsTva * dTva / 100;
+            string result = " le prix TTc du produit " + nomProduit + " est " + prixTtc;
+            Console.WriteLine(result);
+
+        }
+    }
+}
