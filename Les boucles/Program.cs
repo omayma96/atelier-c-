791d2751c580668816c6ba4817dea2de726bafc0
omayma hajjami <omayma.hajjami@gmail.com>
2020-12-01 16:08:37 +0100

    la multiplication inférieur à 30

--- Les boucles/Program.cs
+++ Les boucles/Program.cs
@@ -6,7 +6,18 @@ namespace Les_boucles
     {
         static void Main(string[] args)
         {
-            
+            int a = 1;
+            int c = 1;
+           Random random = new Random();
+            int randomNumber;
+            randomNumber = random.Next(2, 200);  
+            while (a <= 30)
+            {
+                c = a * randomNumber;
+                Console.WriteLine(c);
+                a++;
+            }
+
         }
     }
 }
